28cc8d40c75e7a61b68ba04e66a71483550dcb89
hamdialsakkaf <hamdisaqqaf@gmail.com>
2022-08-26 22:41:47 +0300

    first commit

---
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { ScrollView } from "react-native";
+import { List } from "react-native-paper";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+import { SafeArea } from "../../../components/utility/safe-area.component";
+
+export const RestaurantDetailScreen = ({ route }) => {
+  const [breakfastExpanded, setBreakfastExpanded] = useState(false);
+  const [lunchExpanded, setLunchExpanded] = useState(false);
+  const [dinnerExpanded, setDinnerExpanded] = useState(false);
+  const [drinksExpanded, setDrinksExpanded] = useState(false);
+
+  const { restaurant } = route.params;
+  return (
+    <SafeArea>
+      <RestaurantInfoCard restaurant={restaurant} />
+      <ScrollView>
+        <List.Accordion
+          title="اطارات"
+          left={(props) => <List.Icon {...props} icon="bread-slice" />}
+          expanded={breakfastExpanded}
+          onPress={() => setBreakfastExpanded(!breakfastExpanded)}
+        >
+          <List.Item title="اطارات سيارات" />
+          <List.Item title="اطارات كبيرة" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="زيوت المحركات"
+          left={(props) => <List.Icon {...props} icon="hamburger" />}
+          expanded={lunchExpanded}
+          onPress={() => setLunchExpanded(!lunchExpanded)}
+        >
+          <List.Item title="زيوت محركات الصقر" />
+          <List.Item title="زيوت محركات اس ار اس" />
+          <List.Item title="فلاتر" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="شحوم"
+          left={(props) => <List.Icon {...props} icon="food-variant" />}
+          expanded={dinnerExpanded}
+          onPress={() => setDinnerExpanded(!dinnerExpanded)}
+        >
+          <List.Item title="شحم الصقر" />
+          <List.Item title="شحم فاج" />
+          <List.Item title="" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="فلاتر ساكورا"
+          left={(props) => <List.Icon {...props} icon="cup" />}
+          expanded={drinksExpanded}
+          onPress={() => setDrinksExpanded(!drinksExpanded)}
+        >
+          <List.Item title="فلاتر زيت سيارات" />
+          <List.Item title="فلاتر زيت قواطر " />
+          <List.Item title="فلاتر هواء" />
+          
+        </List.Accordion>
+      </ScrollView>
+    </SafeArea>
+  );
+};
---
+++ src/services/restaurants/mock/index.js
@@ -0,0 +1,27 @@
+import antwerp from "./antwerp.json";
+import chicago from "./chicago.json";
+import toronto from "./toronto.json";
+import taizstr from "./taizstr.json";
+import san_francisco from "./san_francisco.json";
+
+export const mocks = {
+  "51.219448,4.402464": antwerp,
+  "43.653225,-79.383186": toronto,
+  "15.323663,44.229795": taizstr, 
+  "41.878113,-87.629799": chicago,
+  "37.7749295,-122.4194155": san_francisco,
+};
+
+export const mockImages = [
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-oranges-ice-600x750.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2020/08/detail-of-pavlova-strawberry-piece-of-cake-600x800.jpg",
+  "https://i.imgur.com/QZ3NcZl.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-pancakes-600x750.jpg",
+  "https://i.imgur.com/nReo2T2.png",
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/messy-pizza-on-a-black-table-600x400.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/pizza-ready-for-baking-600x400.jpg",
+  "https://i.imgur.com/ZOYr2B9m.jpg",
+  "https://www.foodiesfeed.com/wp-content/uploads/2020/05/suco-de-limao-com-slash-683x1024.jpg.webp",
+
+];
